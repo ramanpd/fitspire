da4022520f2813fcd8a270269a88d50763ac8c55
Raman Prasad <ramanrpd@gmail.com>
2019-03-13 16:02:04 +0800

    Facebook login: Login and log out working on separate buttons, switch screens

---
+++ FitSpire/FitSpire/SecondViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.swift
+//  FitSpire
+//
+//  Created by Raman Prasad on 13/03/19.
+//  Copyright © 2019 Connor Clancy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    
+}
