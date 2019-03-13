c05e8bdff029dd016c0764e8bc1522f603dd1914
ConnorClancy <connor.clancy@ucdconnect.ie>
2019-03-13 21:15:21 +0800

    Added UI groundwork and Walking tracker exercise

--- FitSpire/FitSpire/WalkingVC.swift
+++ FitSpire/FitSpire/WalkingVC.swift
@@ -8,25 +8,50 @@
 
 import UIKit
 import CoreMotion
-class WalkingVC: UIViewController {
+class WalkingVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return distanceOptions.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(distanceOptions[row]) + " km"
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        distanceSelection = distanceOptions[row]
+        print(distanceOptions[row])
+    }
+    
     private let activityManager = CMMotionActivityManager()
     private let pedometer = CMPedometer()
     // MARK: - Properties
     @IBOutlet weak var GoButton: UIButton!
     @IBOutlet weak var StepCounter: UILabel!
+    @IBOutlet weak var DistanceChoice: UIPickerView!
+    var distanceOptions: [Int] = [Int]()
+    var distanceSelection = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        distanceOptions = [1, 2, 3, 4]
+        self.DistanceChoice.delegate = self
+        self.DistanceChoice.dataSource = self
+    
         // Do any additional setup after loading the view.
     }
     
     // MARK: - Actions
     @IBAction func onPress(_ sender: UIButton) {
         if(CMPedometer.isStepCountingAvailable() ){
+            //DistanceChoice.removeFromSuperview()
+            //can't really test this without an actual iPhone so commenting it for now
             beginCounting()
         }else{
             StepCounter.text = "No Pedometer Found"
+        
         }
     }
     /*
@@ -44,7 +69,9 @@ class WalkingVC: UIViewController {
             guard let pedometerData = pedometerData, error == nil else { return }
             
             DispatchQueue.main.async {
-                self?.StepCounter.text = "Step Count: " +  pedometerData.numberOfSteps.stringValue
+                self?.StepCounter.text = "Goal: " + String(self?.distanceSelection ?? 1*1000 - pedometerData.numberOfSteps.intValue)
+                //requires slef? and ?? 1 to safely unwrap from Int? to int...
+                //Not really aware of what any of that means personally
             }
         }
     }
