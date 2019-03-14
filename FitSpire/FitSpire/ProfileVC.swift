efc6357bd3f96c0581b6a58f915230b07ea3bebb
Raman Prasad <ramanrpd@gmail.com>
2019-03-14 12:40:17 +0800

    Facebook Profile picture and Information successfully added to the profile view

--- FitSpire/FitSpire/ProfileVC.swift
+++ FitSpire/FitSpire/ProfileVC.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
-
+import FBSDKLoginKit
 class ProfileVC: UIViewController {
+    var dict : [String : AnyObject]!
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var profilePicImageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let loginManager : FBSDKLoginManager = FBSDKLoginManager()
+        self.getFBUserData()
+        
     }
+    
+    func getFBUserData(){
+        if((FBSDKAccessToken.current()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    self.dict = result as! [String : AnyObject]
+                    print(result!)
+                    print("CUNT")
+                    
+                    var profileName = self.dict?["name"]
+                    var profileId=self.dict?["id"]
+                    var profilePicture = self.dict?["picture"]
+                    var pictureURL = profilePicture?["url"]
+                    self.nameTextField.text = profileName as? String
+                    let url = NSURL(string: "https://graph.facebook.com/\(profileId!)/picture?type=large&return_ssl_resources=1")
+                    self.profilePicImageView.image = UIImage(data: NSData(contentsOf: url! as URL)! as Data)
+                    print(self.dict)
+                }
+            })
+        }
+    }
+    
     
 
     /*
