cd378f3286d8f1402d32c17e5dcec4f4e35d6972
Raman Prasad <ramanrpd@gmail.com>
2019-03-20 14:28:47 +0800

    added new VC for Pushups

---
+++ FitSpire/FitSpire/PushUpsVC.swift
@@ -0,0 +1,31 @@
+//
+//  PushUpsVC.swift
+//  FitSpire
+//
+//  Created by Raman Prasad on 20/03/19.
+//  Copyright © 2019 Connor Clancy. All rights reserved.
+//
+
+import UIKit
+
+class PushUpsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
