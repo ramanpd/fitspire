d59d052900f1aa6379bd10ec2758c8736df9d06f
ConnorClancy <connor.clancy@ucdconnect.ie>
2019-03-20 13:51:49 +0800

    Updated Pedometer to include a progression meter and basic completion notification

--- FitSpire/FitSpire/ProfileVC.swift
+++ FitSpire/FitSpire/ProfileVC.swift
@@ -1,96 +1,96 @@
+////
+////  ProfileVC.swift
+////  FitSpire
+////
+////  Created by Connor Clancy on 13/03/2019.
+////  Copyright © 2019 Connor Clancy. All rights reserved.
+////
 //
-//  ProfileVC.swift
-//  FitSpire
+//import UIKit
+//import FBSDKLoginKit
+//import Charts
 //
-//  Created by Connor Clancy on 13/03/2019.
-//  Copyright © 2019 Connor Clancy. All rights reserved.
 //
-
-import UIKit
-import FBSDKLoginKit
-import Charts
-
-
-class ProfileVC: UIViewController {
-    var dict : [String : AnyObject]!
-    
-    //MARK: Properties
-    
-    @IBOutlet weak var barChartView: BarChartView!
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var nameTextField: UITextField!
-    
-    @IBOutlet weak var profilePicImageView: UIImageView!
-    
-
-    var months: [String]!
-    var noOfGamesPlayedMonthwise: [Int]!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        let noOfGamesPlayedMonthwise = [30,1,24,53,66,77,4,3,2,1,6,8]
-        setBarChart(dataPoints: months, values: noOfGamesPlayedMonthwise)
-        // Do any additional setup after loading the view.
-        scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.view.frame.height+100)
-        self.getFBUserData()
-        
-    }
-    func setBarChart(dataPoints: [String], values: [Int])
-    {
-        barChartView.noDataText = "No Game played yet to generate the data"
-        var dataEntries: [BarChartDataEntry] = []
-        
-        for i in 0..<dataPoints.count {
-            let dataEntry = BarChartDataEntry(x: Double(i), yValues: [Double(values[i])])
-            dataEntries.append(dataEntry)
-        }
-        let chartDataSet = BarChartDataSet(entries: dataEntries, label: "Games Played")
-        let chartData = BarChartData(dataSet: chartDataSet)
-        self.barChartView.data = chartData
-        chartDataSet.stackLabels=months
-        
-        chartDataSet.colors = ChartColorTemplates.colorful()
-        barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
-
-        
-    }
-    func getFBUserData(){
-        if((FBSDKAccessToken.current()) != nil){
-            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
-                if (error == nil){
-                    self.dict = result as! [String : AnyObject]
-                    print(result!)
-                    print("CUNT")
-                    
-                    var profileName = self.dict?["name"]
-                    var profileId=self.dict?["id"]
-                    var profilePicture = self.dict?["picture"]
-                    var pictureURL = profilePicture?["url"]
-                    self.nameTextField.text = profileName as? String
-                    let url = NSURL(string: "https://graph.facebook.com/\(profileId!)/picture?type=large&return_ssl_resources=1")
-                    self.profilePicImageView.image = UIImage(data: NSData(contentsOf: url! as URL)! as Data)
-                    print(self.dict)
-                    self.profilePicImageView.layer.borderWidth=1.0
-                    self.profilePicImageView.layer.masksToBounds = false
-                    self.profilePicImageView.layer.borderColor = UIColor.white.cgColor
-                    self.profilePicImageView.layer.cornerRadius = self.profilePicImageView.frame.size.height/2
-                    self.profilePicImageView.clipsToBounds = true
-                }
-            })
-        }
-    }
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+//class ProfileVC: UIViewController {
+//    var dict : [String : AnyObject]!
+//    
+//    //MARK: Properties
+//    
+//    @IBOutlet weak var barChartView: BarChartView!
+//    @IBOutlet weak var scrollView: UIScrollView!
+//    @IBOutlet weak var nameTextField: UITextField!
+//    
+//    @IBOutlet weak var profilePicImageView: UIImageView!
+//    
+//
+//    var months: [String]!
+//    var noOfGamesPlayedMonthwise: [Int]!
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+//        let noOfGamesPlayedMonthwise = [30,1,24,53,66,77,4,3,2,1,6,8]
+//        setBarChart(dataPoints: months, values: noOfGamesPlayedMonthwise)
+//        // Do any additional setup after loading the view.
+//        scrollView.contentSize = CGSize(width: self.view.frame.width, height: self.view.frame.height+100)
+//        self.getFBUserData()
+//        
+//    }
+//    func setBarChart(dataPoints: [String], values: [Int])
+//    {
+//        barChartView.noDataText = "No Game played yet to generate the data"
+//        var dataEntries: [BarChartDataEntry] = []
+//        
+//        for i in 0..<dataPoints.count {
+//            let dataEntry = BarChartDataEntry(x: Double(i), yValues: [Double(values[i])])
+//            dataEntries.append(dataEntry)
+//        }
+//        let chartDataSet = BarChartDataSet(entries: dataEntries, label: "Games Played")
+//        let chartData = BarChartData(dataSet: chartDataSet)
+//        self.barChartView.data = chartData
+//        chartDataSet.stackLabels=months
+//        
+//        chartDataSet.colors = ChartColorTemplates.colorful()
+//        barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
+//
+//        
+//    }
+//    func getFBUserData(){
+//        if((FBSDKAccessToken.current()) != nil){
+//            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+//                if (error == nil){
+//                    self.dict = result as! [String : AnyObject]
+//                    print(result!)
+//                    print("CUNT")
+//                    
+//                    var profileName = self.dict?["name"]
+//                    var profileId=self.dict?["id"]
+//                    var profilePicture = self.dict?["picture"]
+//                    var pictureURL = profilePicture?["url"]
+//                    self.nameTextField.text = profileName as? String
+//                    let url = NSURL(string: "https://graph.facebook.com/\(profileId!)/picture?type=large&return_ssl_resources=1")
+//                    self.profilePicImageView.image = UIImage(data: NSData(contentsOf: url! as URL)! as Data)
+//                    print(self.dict)
+//                    self.profilePicImageView.layer.borderWidth=1.0
+//                    self.profilePicImageView.layer.masksToBounds = false
+//                    self.profilePicImageView.layer.borderColor = UIColor.white.cgColor
+//                    self.profilePicImageView.layer.cornerRadius = self.profilePicImageView.frame.size.height/2
+//                    self.profilePicImageView.clipsToBounds = true
+//                }
+//            })
+//        }
+//    }
+//    
+//    
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
+//}
