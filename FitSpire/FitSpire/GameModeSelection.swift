786a287e0629f1732a18c78dc8965cede0158b3e
Raman Prasad <ramanrpd@gmail.com>
2019-04-03 15:50:59 +0800

    Basic GameModeSelector added

---
+++ FitSpire/FitSpire/GameModeSelection.swift
@@ -0,0 +1,30 @@
+//
+//  GameModeSelection.swift
+//  FitSpire
+//
+//  Created by Raman Prasad on 03/04/19.
+//  Copyright © 2019 Connor Clancy. All rights reserved.
+//
+
+import UIKit
+
+class GameModeSelection: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
